630ab89285fbe8df86e6e5199dc177dc36bca8a0
nambags <nambags@gmail.com>
2022-12-02 02:56:31 -0500

    Theta check in primitives

--- src/planner.cpp
+++ src/planner.cpp
@@ -118,29 +118,31 @@ void Planner::expand_node(int idx) {
     auto primitives = got->second;
 
     for (auto primitive : primitives) {
-        bool primitive_flag = true;
-        for (auto c : primitive.collision_cell) {
-            int del_x = c.i;
-            int del_y = c.j;
-            int curr_x = x + del_x;
-            int curr_y = y + del_y;
+        if (primitive.start.theta == theta) {
+            bool primitive_flag = true;
+            for (auto c : primitive.collision_cell) {
+                int del_x = c.i;
+                int del_y = c.j;
+                int curr_x = x + del_x;
+                int curr_y = y + del_y;
 
-            if (map.is_obstacle(curr_x, curr_y)) {
-                primitive_flag = false;
-                break;
+                if (map.is_obstacle(curr_x, curr_y)) {
+                    primitive_flag = false;
+                    break;
+                }
             }
-        }
-        
-        Point p = {x + primitive.end.x, y + primitive.end.y, primitive.end.theta};
+            
+            Point p = {x + primitive.end.x, y + primitive.end.y, primitive.end.theta};
 
-        int new_idx = get_index(p.x, p.y, p.theta);
-        if (primitive_flag) {
-            double new_g = get_g(new_idx);
+            int new_idx = get_index(p.x, p.y, p.theta);
+            if (primitive_flag) {
+                double new_g = get_g(new_idx);
 
-            double g = get_g(idx) + step_cost(primitive.idx);
-            if (new_g > g) {
-                double h = get_heuristic(p.x, p.y, p.theta);
-                graph.add_node(new_idx, p, g, h, idx, primitive.idx);
+                double g = get_g(idx) + step_cost(primitive.idx);
+                if (new_g > g) {
+                    double h = get_heuristic(p.x, p.y, p.theta);
+                    graph.add_node(new_idx, p, g, h, idx, primitive.idx);
+                }
             }
         }
     }
